a5dc2a88576aaad2c5321039bdafdc093047c7c5
juxhin31 <juxhinwork@gmail.com>
2022-04-15 10:18:13 +0200

    Gjysma E videos

---
+++ Laracast/app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Models;
+
+use App\Models\Post;
+
+
+class Post
+
+{
+    public static function find($slug)
+    {
+        if (! file_exists($path = resource_path("posts/{$slug}.html"))) {
+            throw new ModelNotFoundException();
+    
+    }
+
+    return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path)); 
+       
+
+
+    
+
+
+      
+
+    }
+
+
+}
