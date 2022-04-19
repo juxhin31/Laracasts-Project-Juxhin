2503fd1cdb7e12bedafd448b5168ee968d50437a
juxhin31 <juxhinwork@gmail.com>
2022-04-19 16:54:03 +0200

    video14
    balde basics

--- Laracast/resources/views/posts.blade.php
+++ Laracast/resources/views/posts.blade.php
@@ -7,21 +7,22 @@
 
 
 <body>
-    <?php foreach ($posts as $post) : ?>
+    @foreach ($posts as $post)
+    
 
-    <article>
+    <article class="{{ $loop->even ? 'foobar' : ''}}">
 
         <h1>
-            <a href="/posts/<?= $post->slug; ?>">
+            <a href="/posts/{{ $post->slug }}">
 
 
-            <?= $post->title; ?>
+            {{ $post->title }} 
     </a>
         </h1>
 
         <div>
-            <?= $post->excerpt; ?>
+        {{ $post->excerpt }}
     </div>
     </article>
-    <?php endforeach; ?>
+    @endforeach
 </body>
