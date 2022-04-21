a4ac24f48de5b3c39fb00207226c5f1501108ba0
juxhin31 <juxhinwork@gmail.com>
2022-04-21 12:49:45 +0200

    video16
    fn find($slug)
    fn findOrFail

--- Laracast/app/Models/Post.php
+++ Laracast/app/Models/Post.php
@@ -54,10 +54,20 @@ class Post
         
     }
 
-    public static function find($slug)
+    public static function find($slug) 
     {
         return static::all()->firstWhere('slug', $slug);
 
-    
     }
+
+    public static function findOrFail($slug) 
+    {
+        $post = static::find($slug);
+
+        if (! $post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
     }
+}
