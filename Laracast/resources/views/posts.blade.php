93d34cc84353c375f8398dfc12dbd0be860b5e4b
juxhin31 <juxhinwork@gmail.com>
2022-04-25 17:30:15 +0200

    video23

--- Laracast/resources/views/posts.blade.php
+++ Laracast/resources/views/posts.blade.php
@@ -5,10 +5,10 @@
     <article>
 
         <h1>
-            <a href="/posts/{{ $post->id }}">
+            <a href="/posts/{{ $post->slug }}">
 
 
-                {{ $post->title }}
+                {!! $post->title !!}
             </a>
         </h1>
 
